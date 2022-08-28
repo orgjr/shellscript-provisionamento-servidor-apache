104e7a111f278fe77eb714d6d9cc3f3238353993
orgjr <orgj@vivaldi.net>
2022-08-28 15:46:58 -0300

    first commit

---
+++ prov-servapache.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "baixando e atualizando pacotes"
+apt-get update -y
+apt-get upgrade -y
+apt-get install unzip -y
+apt-get install apache2 -y
+
+echo "baixando arquivos da aplicação web"
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio-main
+cp -R * /var/www/html
+
+echo "Fim da configuração do servidor web com Apache"
